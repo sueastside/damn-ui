3a638f6bd0bb594820de421e4676c8e44b06f5ab
sueastside <sueastside@gmail.com>
2014-01-29 17:53:48 +0100

    Added NotificationStream

---
+++ app/scripts/components/notificationstream.js
@@ -0,0 +1,49 @@
+/** @jsx React.DOM */
+
+/*
+    This will be used as a reference module.
+*/
+
+(function() {
+    "use strict";
+
+    var Notification = React.createClass({
+        render: function() {
+            return (
+                <li>
+                    <h3>{this.props.data.timesince}</h3>
+                    <span>{this.props.data.__str__}</span>
+                </li>
+            );
+        }
+    });
+
+    
+    var NotificationStream = React.createClass({
+        loadCommentsFromServer: function() {
+            var component = this;
+            var data_xhr = Request(this.props.url);
+            data_xhr.done(function( data ) {
+                component.setState({data: data});
+            });
+          },
+        getInitialState: function() {
+            return {data: []};
+        },
+        componentWillMount: function() {
+            this.loadCommentsFromServer();
+            setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+        },
+        render: function() {
+            return (
+                <ul className="workspace-list">
+                    {this.state.data.map(function(itm)  {
+                        return <Notification data={itm} />
+                    })}
+                </ul>
+            );
+        }
+    });
+    window.NotificationStream = NotificationStream;
+
+} ());
--- app/scripts/dashboard.js
+++ app/scripts/dashboard.js
@@ -1,91 +1,50 @@
 /** @jsx React.DOM */
 
 /*
-	This will be used as a reference module.
+    This will be used as a reference module.
 */
 
 (function() {
-	"use strict";
+    "use strict";
 
-	/*Define react modules of the module*/
-	var TaskList = React.createClass({
-		render: function() {
-			return (
-				<ul className="workspace-list">
-					{this.props.data.tasks.map(function(itm)  {
-						return <Task data={itm} />
-					})}
-				</ul>
-			);
-		}
-	});
-
-	var Notification = React.createClass({
-		render: function() {
-			return (
-				<li>
-					<h3>{this.props.data.timesince}</h3>
-					<span>{this.props.data.__str__}</span>
-				</li>
-			);
-		}
-	});
-
-	var Menu = React.createClass({
-		render: function() {
-
-		}
-	});
-    
-
+    /*Define react modules of the module*/
     var Task = React.createClass({
-		render: function() {
+        render: function() {
             console.log(this.props.data);
-			return (
-				<li key={this.props.data.id} data-url={this.props.data.url}>
-					<h3>{this.props.data.summary}</h3>
-					<span>{this.props.data.description}</span>
-				</li>
-			);
-		}
-	});
+            return (
+                <li key={this.props.data.id} data-url={this.props.data.url}>
+                    <h3>{this.props.data.summary}</h3>
+                    <span>{this.props.data.description}</span>
+                </li>
+            );
+        }
+    });
     
     var Project = React.createClass({
-		render: function() {
+        render: function() {
             console.log(this.props.data);
-			return (
-				<li key={this.props.data.id} data-url={this.props.data.url}>
-					<h3>{this.props.data.name}</h3>
-					<span>{this.props.data.is_following}</span>
-				</li>
-			);
-		}
-	});
+            return (
+                <li key={this.props.data.id} data-url={this.props.data.url}>
+                    <h3>{this.props.data.name}</h3>
+                    <span>{this.props.data.is_following}</span>
+                </li>
+            );
+        }
+    });
 
 
-	/* Loading class
-	 * This will be used when loading this specific page
-	 * Things like loading/caching data will be handled here.
-	 * This is called in main.js LoadView function.
-	 */
-	window.Dashboard = function(state) {
-/*
-		var user_activity = Request("/users/1/activity");
-    
-		user_activity.done(function( xhr_data ) {
-			
-			var data = {
-				tasks: xhr_data
-			}
-
-			React.renderComponent(
-				<TaskList data={data}></TaskList>
-				$("#workspace-area")[0]
-			);
-*/      if (state=='tasks') {
+    /* Loading class
+     * This will be used when loading this specific page
+     * Things like loading/caching data will be handled here.
+     * This is called in main.js LoadView function.
+     */
+    window.Dashboard = function(state) {
+        if (state=='tasks') {
             React.renderComponent(<FilteredList type={Task} url="/tasks/"></FilteredList>, $("#workspace-area")[0]);
         } else if (state=='projects') {
             React.renderComponent(<FilteredList type={Project} url="/projects/"></FilteredList>, $("#workspace-area")[0]);
+        } else {
+            React.renderComponent(<NotificationStream url="/users/1/activity/" pollInterval={2000}></NotificationStream>, $("#workspace-area")[0]);
         }
     };
 } ());
