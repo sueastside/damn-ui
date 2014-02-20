6fa2f41f6c5f92e6e04a073fdc5c8d73e3e41d37
sueastside <sueastside@gmail.com>
2014-02-20 18:43:52 +0100

    Travis

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -20,12 +20,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   
   cd dist/
   git init
-  git config user.name "Travis"
-  git config user.email "travis@travis-ci.org"
+  #git config user.name "Travis"
+  #git config user.email "travis@travis-ci.org"
+  
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
   
   touch .nojekyll
   git add .
   git commit -m "Deployed to Github Pages"
   #git push --force --quiet "https://${GH_TOKEN}@github.com/sueastside/damn.git" master:gh-pages > /dev/null 2>&1
-  git push --force --quiet "https://${GH_TOKEN}@github.com/sueastside/damn.git" master:gh-pages
+  git push --force "https://${GH_TOKEN}@github.com/sueastside/damn.git" master:gh-pages
+  
+  echo -e "Done updating gh-pages\n"
 fi
