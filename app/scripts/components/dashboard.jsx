6a3f1d7c9d36a975a5bcc229e5548822bbd3acee
sueastside <sueastside@gmail.com>
2014-07-14 18:25:11 +0200

    Added create task and jsplumb

--- app/scripts/components/dashboard.jsx
+++ app/scripts/components/dashboard.jsx
@@ -175,7 +175,6 @@
         console.log('window.Dashboard: name '+name);
         console.log('window.Dashboard: state '+context);
         
-        //React.renderComponent(<Editor url={"/projects/1/tasks/"} />, $("#workspace-area")[0]);
         //return;
 
         if (name=='tasks') {
@@ -187,3 +186,135 @@
         }
     };
 } ());
+
+
+/*
+
+;(function() {
+
+	jsPlumb.ready(function() {
+		// setup some defaults for jsPlumb.	
+		var instance = jsPlumb.getInstance({
+			Endpoint : ["Dot", {radius:2}],
+			HoverPaintStyle : {strokeStyle:"#1e8151", lineWidth:2 },
+			ConnectionOverlays : [
+				[ "Arrow", { 
+					location:1,
+					id:"arrow",
+                    length:14,
+                    foldback:0.8
+				} ]
+			],
+			Container:"statemachine-demo"
+		});
+
+		var windows = jsPlumb.getSelector(".statemachine-demo .w");
+        
+        var g = new dagre.Digraph();
+        
+        windows.each(function(i, el) {
+            var info = {};
+            info['label'] = $(el).text();
+            info['width'] = $(el).width();
+            info['height'] = $(el).height();
+            if ($(el).attr('id')=='create') {
+                info['rank'] = 'min'; // max, min, same_
+            } else if ($(el).attr('id')=='closed') {
+                info['rank'] = 'max'; // max, min, same_
+            } else {
+                //info['rank'] = 'same_';
+            }
+            console.log(info);
+            g.addNode($(el).attr('id'), info);
+        });
+
+        // initialise draggable elements.  
+		instance.draggable(windows, {containment:"parent"});
+
+        // bind a click listener to each connection; the connection is deleted. you could of course
+		// just do this: jsPlumb.bind("click", jsPlumb.detach), but I wanted to make it clear what was
+		// happening.
+		instance.bind("click", function(c) { 
+			instance.detach(c); 
+		});
+
+		// bind a connection listener. note that the parameter passed to this function contains more than
+		// just the new connection - see the documentation for a full list of what is included in 'info'.
+		// this listener sets the connection's internal
+		// id as the label overlay's text.
+        instance.bind("connection", function(info) {
+			//info.connection.getOverlay("label").setLabel(info.connection.id);
+            console.log(info.connection);
+            var connections = instance.getConnections({source:info.source, target:info.source});
+            console.log(connections);
+            if (connections.length > 1) {
+                instance.detach(info.connection);
+            }
+            g.addEdge(null, info.sourceId,   info.targetId);
+            //connections[0].reapplyTypes({ connector:[ "Straight", {} ]});
+        });
+
+		// suspend drawing and initialise.
+		instance.doWhileSuspended(function() {
+
+			// make each ".ep" div a source and give it some parameters to work with.  here we tell it
+			// to use a Continuous anchor and the StateMachine connectors, and also we give it the
+			// connector's paint style.  note that in this demo the strokeStyle is dynamically generated,
+			// which prevents us from just setting a jsPlumb.Defaults.PaintStyle.  but that is what i
+			// would recommend you do. Note also here that we use the 'filter' option to tell jsPlumb
+			// which parts of the element should actually respond to a drag start.
+			instance.makeSource(windows, {
+				filter:".ep",				// only supported by jquery
+				anchor:"Continuous",
+				//connector:[ "StateMachine", { curviness:20 } ],
+                //connector:[ "StateMachine", { curviness:0, proximityLimit: 800 } ],
+                connector:[ "Flowchart", {} ],
+				connectorStyle:{ strokeStyle:"#5c96bc", lineWidth:2, outlineColor:"transparent", outlineWidth:4 },
+				maxConnections:5,
+				onMaxConnections:function(info, e) {
+					alert("Maximum connections (" + info.maxConnections + ") reached");
+				}
+			});						
+
+			// initialise all '.w' elements as connection targets.
+	        instance.makeTarget(windows, {
+				dropOptions:{ hoverClass:"dragHover" },
+				anchor:"Continuous"				
+			});
+
+			// and finally, make a couple of connections
+			instance.connect({ source:"create", target:"open" });
+            instance.connect({ source:"open", target:"resolved" });
+            instance.connect({ source:"open", target:"closed" });
+            instance.connect({ source:"open", target:"inprogress" });
+
+            instance.connect({ source:"resolved", target:"closed" });
+            instance.connect({ source:"resolved", target:"reopened" });
+            
+            instance.connect({ source:"inprogress", target:"open" });
+            instance.connect({ source:"inprogress", target:"resolved" });
+            instance.connect({ source:"inprogress", target:"closed" });
+            
+            instance.connect({ source:"reopened", target:"closed" });
+            instance.connect({ source:"reopened", target:"resolved" });
+            instance.connect({ source:"reopened", target:"inprogress" });
+
+            
+            console.log('INIT');
+            var layout = dagre.layout().nodeSep(50).edgeSep(50).rankSep(100).rankDir("LR").run(g);
+            console.log(layout);
+            layout.eachNode(function(u, value) {
+                console.log("Node " + u + ": " + JSON.stringify(value));
+                var offset = {top: (value.y - value.height/2)+50, left:(value.x - value.width/2)+200};
+                console.log(offset);
+                $('#'+u).css(offset);
+            });
+            layout.eachEdge(function(e, u, v, value) {
+                console.log("Edge " + u + " -> " + v + ": " + JSON.stringify(value));
+            });
+
+		});
+
+	});
+})();
+*/
